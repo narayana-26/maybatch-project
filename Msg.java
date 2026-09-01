b0dee8f7d3639ffb0a08d78e06a29d41e71b4131
narayana-26 <narayanaakula26@gmail.com>
2026-09-01 09:06:12 +0530

    Update Msg.java

--- Msg.java
+++ Msg.java
@@ -1,8 +1,6 @@
 public class Msg {
 	public static void main(String[] args) {
 		System.out.println("Hello Good Evening:");
-		System.out.println("Hello Good Morning:");
-		System.out.println("Hello Good Afternoon:");
-		System.out.println("Hello Good Evening:");
+		System.out.println("Hello Good Morning:");	
 	}
 }
